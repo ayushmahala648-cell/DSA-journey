a850cd06d9d1282adc29435dbc6a2f97e22f291d
ayush <ayushmahala648@gmail.com>
2026-04-26 17:23:39 +0530

    Time: 0 ms (100%), Space: 15.8 MB (33.07%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,35 +1,30 @@
-static const bool __boost = [] () {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return true;
-}();
-
-const size_t BUFFER_SIZE = 0x8000000; 
-alignas (std::max_align_t) char buffer[BUFFER_SIZE];
-size_t buffer_pos = 0;
-
-void* operator new (size_t size) {
-    constexpr std::size_t alignment = alignof(std::max_align_t);
-
-    if (buffer_pos % alignment != 0) {
-        buffer_pos += alignment - (buffer_pos % alignment);
-    }
-    
-    void* ptr = &buffer[buffer_pos];
-    buffer_pos += size;
-    return ptr;
-}
-
-void operator delete (void*, unsigned long) noexcept {}
-void operator delete (void*) noexcept {}
-void operator delete[] (void*) noexcept {}
-void operator delete[] (void*, size_t) noexcept {}
-
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin() , nums.end());
+        int n= nums.size();
+        int ind1 = -1;
+        for(int i=n-1 ; i>0 ; i--){
+            if(nums[i] > nums[i-1]){
+                ind1 = i-1;
+                break;
+            }   
+        }
+
+        if(ind1 == -1){
+            sort(nums.begin() , nums.end());
+        }
+        else{
+            for(int j=n-1 ; j>ind1 ; j--){
+                if(nums[j]>nums[ind1]){
+                    swap(nums[ind1] , nums[j]);
+                    break;
+                }    
+            }
+            sort(nums.begin()+ind1+1 , nums.end());
+        }
+        
+        
+
         
     }
 };
