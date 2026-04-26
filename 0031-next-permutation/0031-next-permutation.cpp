dea3a8df5079b7c24781b7bd2d2e28b35c562d93
ayush <ayushmahala648@gmail.com>
2026-04-26 14:32:40 +0530

    Time: 0 ms (100%), Space: 15.1 MB (100%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,3 +1,31 @@
+static const bool __boost = [] () {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+const size_t BUFFER_SIZE = 0x8000000; 
+alignas (std::max_align_t) char buffer[BUFFER_SIZE];
+size_t buffer_pos = 0;
+
+void* operator new (size_t size) {
+    constexpr std::size_t alignment = alignof(std::max_align_t);
+
+    if (buffer_pos % alignment != 0) {
+        buffer_pos += alignment - (buffer_pos % alignment);
+    }
+    
+    void* ptr = &buffer[buffer_pos];
+    buffer_pos += size;
+    return ptr;
+}
+
+void operator delete (void*, unsigned long) noexcept {}
+void operator delete (void*) noexcept {}
+void operator delete[] (void*) noexcept {}
+void operator delete[] (void*, size_t) noexcept {}
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
