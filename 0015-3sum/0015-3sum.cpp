143511770c3ba62bf5984f76c6311097fca615d5
ayush <ayushmahala648@gmail.com>
2026-05-03 01:34:17 +0530

    Time: 56 ms (23.22%), Space: 31 MB (22.72%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,4 +1,19 @@
 class Solution {
+    struct Freq {
+        uint64_t data[(100'001 + 15) / 16] = {};
+
+        inline uint64_t get(uint x, uint sign) const {
+            uint shift = ((x & 15) << 2) | (sign << 1);
+            return (data[x >> 4] >> shift) & 3;
+        }
+
+        inline void add(uint x, uint sign) {
+            uint i = x >> 4;
+            uint shift = ((x & 15) << 2) | (sign << 1);
+            auto v = 3 & ~(data[i] >> shift);
+            data[i] += (uint64_t)(!!v) << shift;
+        }
+    };
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
@@ -34,4 +49,3 @@ public:
         return final;
     }
 };
-const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
