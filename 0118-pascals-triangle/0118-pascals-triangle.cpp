2174549261e93e3ce138e43fc52eb256b24bc270
ayush <ayushmahala648@gmail.com>
2026-05-02 13:56:20 +0530

    Time: 0 ms (100%), Space: 9.6 MB (62.39%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int pascal(int r , int i){
+        long long val=1;
+        long long div=1;
+        if(i >= r/2){
+            i = r-i-1;
+        }
+
+        for(int k=i ; k>0 ; k--){
+            r--;
+            val *= r;
+            div *=k;
+        }
+
+        return val/div;
+    }
+    void pascalrows (vector <int> &currentrow , int r){
+        
+        for(int i=0 ; i<r ; i++){
+            currentrow.emplace_back(pascal(r,i));
+        }
+        
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+
+        for(int r=1 ; r<=numRows ; r++){
+            vector<int> currentrow;
+            pascalrows(currentrow , r);
+            ans.emplace_back(currentrow);
+        }
+        return ans;
+    }
+};
