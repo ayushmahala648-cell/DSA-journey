81547e8d0432383c3001a56c02607dd5a7a4589f
ayush <ayushmahala648@gmail.com>
2026-08-07 00:38:22 +0530

    Time: 0 ms (100%), Space: 11.3 MB (18.94%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n = nums.size();
-        vector <int> temp = nums;
-
-        for(int i=0 ; i<n ; i++){
-            for(int j =i ; j>0 ; j--){
-                if(nums[j]<nums[j-1]){
-                    swap(nums[j] , nums[j-1]);
-                }
+        int down = 0;
+        for(int i=1 ; i<nums.size() ; i++){
+            if(nums[i] < nums[i-1]){
+                down++;
+                if (down >1) return false;
             }
         }
+        if(nums[nums.size()-1] > nums[0] ) down ++;
 
-        // now nums contains the sorted vector
-        // and the snum contains the original vector
-
-        // myapproach here is it will rotate through every single rotation possible and 
-        // in one of those rotation one num will match and it will increase the cnt 
-        // when cnt is = n then it is exact equal and hence will print n else will try for other loop.
-        int cnt =0;
-        for(int d = 0 ; d<n ; d++){
-            for(int i = 0 ; i<n ; i++){
-                if(nums[i] == temp[(i+d)%n]){
-                    cnt++;
-                }
-            }
-
-            if(cnt == n){
-                    return true;
-            }else{
-                cnt=0;
-            }
-        }
+        if (down ==1 || down ==0) return true;
         return false;
     }
 };
