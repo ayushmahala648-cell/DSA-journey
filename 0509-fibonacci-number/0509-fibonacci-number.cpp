76b5c84ae719e3a3e7d283dd0c844a50f2cff73b
ayush <ayushmahala648@gmail.com>
2026-08-07 00:27:05 +0530

    Time: 0 ms (100%), Space: 8.2 MB (17.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==1 || n==0) return n;
+        vector <int> fib(n+1);
+        
 
-        return fib(n-1) + fib(n-2);
+        for(int i=0 ; i<=n ; i++){
+            if(i==0) fib[0]= 0 ;
+            else if(i==1) fib[1] = 1;
+
+            else {fib[i] = fib[i-1] + fib[i-2];}
+        }
+        return fib[n];
     }
 };
